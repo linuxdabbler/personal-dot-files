4105009ffdea3e2be8ba6e08872e7eac296b3e68
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2020-03-28 14:13:34 +0000

    setup script for my spectrwm config
    moves files around, makes things executable, puts things in the PATH.... basically automates things I'm too lazy to do myself.

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# cat out Xresources file into ~/.Xdefaults to get my urxvt theme
+cat Xresources >> ~/.Xdefaults
+
+# Make baraction.sh executable
+chmod +x baraction.sh
+
+# copy my baraction script into the config folder for spectrwm to call it up from the config file
+cp baraction.sh ~/.config/
+
+# Make a backup of the current config file if there is one, or create a default one to fall back to in the ~/.config directory
+cp ~/.spectrwm.conf ~/.config/spectrwm.conf.bak || sudo cat /etc/spectrwm.conf > ~/.config/spectrwm.conf.bak && sudo cat /usr/share/doc/spectrwm/examples/spectrwm_us.conf >> ~/.config/spectrwm.conf.bak
+
+# Cat out spectrwm.conf to the home folder
+cat spectrwm.conf > ~/.spectrwm.conf
+
+# Make auto wallpaper changer script "wallok" executable
+chmod +x wallok
+
+# Move wallok script to a directory in the default PATH
+sudo mv wallok /usr/local/bin/
+
+cp -r wallpapers ~/Pictures/ || mkdir ~/Pictures && cp -r wallpapers ~/Pictures
+
+mkdir ~/Pictures/wallok/ & mkdir ~/Pictures/shots || exit
+
+
