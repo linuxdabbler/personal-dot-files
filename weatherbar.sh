bbd929040c5bfcef37b033726593076a17fcfa46
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2020-10-17 02:21:23 -0500

    Update weatherbar.sh

--- weatherbar.sh
+++ weatherbar.sh
@@ -4,29 +4,28 @@ weather=$(cat ~/.config/weather.txt | sed 20q | grep value | awk '{print $2 $3}'
 
 temp=$(cat ~/.config/weather.txt | grep temp_F | awk '{print $2}' | sed 's/\"//g' | sed 's/,//')
 
-#echo "$weather $temp"
+nocloud="盛"
+cloud=""
+rain=""
+storm=""
+lotempicon=""
+midtempicon=""
+hitempicon=""
 
-nocloud=""
-cloud=""
-lotempicon=""
-midtempicon=""
-hitempicon=""
-
-# print weather info
+#print weather info
 TEMP() {
 if [ "$temp" -gt 80 ]; then
 	echo -e $hitempicon $temp
 elif [ "$temp" -ge 50 ] && [ "$temp" -le 79 ]; then
 	echo -e $midtempicon $temp
-elif [ "$temp" -le 49 ]; then
-	echo $lotempicon $temp
+elif [ "$temp" -le 49]; then
+	echo -e $lotempicon $temp
 fi
 }
 
-WEATHER() {
-if [ "$weather" = "Clear" ]; then
+WEATHER () {
+if [[ "$weather" = "Clear" ]] || [[ "$weather" = "Sunny" ]]; then
 	echo -e "$nocloud $weather"
-
 else
 	echo -e "$cloud $weather"
 fi
