aa9d6da8fe17314b6b62f45d6adabab9bb149d0b
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2020-10-17 01:56:23 -0500

    Update baraction.sh

--- baraction.sh
+++ baraction.sh
@@ -5,21 +5,12 @@
 # This config can be found on github.com/linuxdabbler
 
 ############################## 
-#	    DATE
-##############################
-
-date() {
-	  date="$(date +"%a, %b %d %R")"
-	    echo -e " $dte "
-    }
-
-############################## 
 #	    DISK
 ##############################
 
 hdd() {
 	  hdd="$(df -h /home | grep /dev | awk '{print $3 " / " $5}')"
-	    echo -e " $hdd "
+	    echo -e " $hdd"
     }
 ##############################
 #	    RAM
@@ -27,7 +18,7 @@ hdd() {
 
 mem() {
 	mem="$(free -h | awk '/Mem:/ {printf $3 "/" $2}')"
-	echo -e " $mem "
+	echo -e " $mem"
 }
 ##############################	
 #	    CPU
@@ -40,7 +31,7 @@ cpu() {
 	        read cpu a b c idle rest < /proc/stat
 		  total=$((a+b+c+idle))
 		    cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-		      echo -e " $cpu% "
+		      echo -e "  $cpu%"
 	      }
 ##############################
 #	    VOLUME
@@ -48,15 +39,15 @@ cpu() {
 
 vol() {
 	vol="$(amixer -D pulse get Master | awk -F'[][]' 'END{ print $4":"$2 }')"
-	echo -e " $vol "
+	echo -e " $vol"
 }
 ##############################
 #	    Packages
 ##############################
 
 pkgs() {
-	pkgs="$(apt-mark showmanual | wc -l)"
-	echo -e " $pkgs "
+	pkgs="$(apt list --installed | wc -l)"
+	echo -e " $pkgs"
 }
 ##############################
 #	    UPGRADES
@@ -64,7 +55,7 @@ pkgs() {
 
 upgrades() {
 	upgrades="$(aptitude search '~U' | wc -l)"
-	echo -e " $upgrades "
+	echo -e " $upgrades"
 }
 ##############################
 #	    VPN
@@ -72,7 +63,7 @@ upgrades() {
 
 vpn() {
 	vpn="$(ip a | grep tun0 | grep inet | wc -l)"
-	echo -e " $vpn "
+	echo -e " $vpn"
 }
 ## WEATHER
 weather() {
@@ -87,14 +78,10 @@ temp() {
 }
 
 
-      SLEEP_SEC=1
+      SLEEP_SEC=2
       #loops forever outputting a line every SLEEP_SEC secs
       while :; do     
-	#This bar is for spectrwm 3.3+
-#    echo "+@fg=5;$(cpu) +@fg=0;| +@fg=2;$(mem) +@fg=0;| +@fg=4;$(pkgs) +@fg=0;| +@fg=1;$(hdd) +@fg=0;| +@fg=3;$(vpn)+@fg=0;| +@fg=1;$(vol) +@fg=0;|+@fg=2; $(weather) $(temp)   +@fg=0;"
-	#This bar has icons and requires font-awesome package to be installed and declared as a secondary font in spectrwm.config
-    echo "+@fn=1; +@fn=0;$(cpu) |  +@fn=1; +@fn=0;$(mem) |  +@fn=1;+@fn=0;$(pkgs) |  +@fn=1;+@fn=0;$(upgrades) |   +@fn=1;+@fn=0;$(hdd) | VPN  +@fn=1;+@fn=0;$(vpn) |  +@fn=1;+@fn=0;$(vol) | $(weather) $(temp)"
+    echo "$(cpu)| $(mem)| $(pkgs)| $(hdd)|嬨 $(vpn)| $(vol)|$(weather) $(temp)|"
 		sleep $SLEEP_SEC
 		done
-
 
